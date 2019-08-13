1bae05bbd1da638017b6d8e5d185b15ecabfd547
Christian Sarnataro <christian.sarnataro@gmail.com>
2019-08-13 20:44:47 +0200

    Created 'client' folder for keeping FE and BE as much separated as possible

--- src/server/api/api.js
+++ src/server/api/api.js
@@ -7,7 +7,9 @@ const days = require('./days')
 
 const API_KEYS = [
   'e6af5b5feb891b272e18f5e2fc0370a6',
-  '60bb82076d33e48d2cb2480bd4a8f897'
+  '60bb82076d33e48d2cb2480bd4a8f897',
+  '15f8bf5641489ec32f66662221933c14',
+  '582cb3a77cc9a6ffd73b06b85c04ad8a'
 ]
 // process.env.DARKSKY_API_KEY;
 const BASE_URL = `https://api.darksky.net/forecast`
@@ -27,8 +29,8 @@ router.use(function timeLog (req, res, next) {
 function getForecast (req, resp) {
   console.log('Received forecast request')
   const location = req.params.location || '45.4754304,9.2110848'
-  const url = `${BASE_URL}/${API_KEYS[Math.round(Math.random())]}/${location}?lang=it&units=si`
-  console.dir(months)
+  const url = `${BASE_URL}/${API_KEYS[1]}/${location}?lang=it&units=si`
+  console.log(url)
   // resp.json(generateFakeForecast(location));
   fetch(url).then((response) => {
     if (response.status !== 200) {

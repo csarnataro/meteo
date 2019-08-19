21b555040e71c808f6434d177856daecd9f2949d
Christian Sarnataro <christian.sarnataro@gmail.com>
2019-08-20 01:30:24 +0200

    Formatted mock file.

--- src/server/api/format-forecast.test.js
+++ src/server/api/format-forecast.test.js
@@ -1,14 +1,13 @@
 const DataPoint = require('data-point')
 const { formatForecast } = require('./format-forecast')
-const data = require ('./__mocks__/api-darksky-forecast')
+const data = require('./__mocks__/api-darksky-forecast')
 
 describe('formatForecast', () => {
   it('should just format', async () => {
-
     const reducer = input => formatForecast(input)
     const datapoint = DataPoint.create()
-    const output = await datapoint.resolve(reducer, data)   
-    console.dir(output, { depth: 10, color: true})
+    const output = await datapoint.resolve(reducer, data)
+    console.dir(output, { depth: 16, color: true })
     expect(output).not.toBeUndefined()
   })
 })
